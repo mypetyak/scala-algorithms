866b08b4154193dccd06ea2580c5964a8c90781a
Christopher Bunn <b11.chris@gmail.com>
2015-10-13 04:11:32 +0000

    Added initial quicksort solution

---
+++ quicksort.md
@@ -0,0 +1,4 @@
+#Quicksort
+
+##Summary:  
+Quicksort is an divide and conquer comparison sort that exhibits O(n*ln(n)) behavior on average. Its worst case O(n^2) occurs when sorting an already-sorted list.  Quicksort is unstable.
---
+++ quicksort.scala
@@ -0,0 +1,56 @@
+/**
+ * Implements an example of Quicksort.
+ *
+ * In English, here's how the algorithm works:
+ * Moving left to right, move all values less than the pivot value to the
+ * left of a floating divide, where the divide always marks the point to 
+ * the left of which all values are less than the pivot. At the end, move
+ * the pivot value to the divide location. Then recurse for the subarrays
+ * on either side of the pivot.
+ **/
+
+object Quicksort {
+  def main(args: Array[String]) {
+    var mess = Array(3, 9, 8, 13, 2, 5, 4);
+
+    quicksort(mess, 0, mess.length-1);
+    mess.foreach( println )
+  }
+
+  /** Swaps two values in an array */
+  def swap(a: Array[Int], pos1: Int, pos2: Int) : Unit = {
+    val stash: Int = a(pos1)
+    a(pos1) = a(pos2)
+    a(pos2) = stash
+  }
+
+  /** Performs recursive quicksort on an array */
+  def quicksort(a: Array[Int], low: Int, hi: Int) : Unit = {
+    if (low < hi) {
+      val p = partition(a, low, hi)
+      quicksort(a, low, p-1)
+      quicksort(a, p+1, hi)
+    }
+  }
+
+  /** Partition an array around some pivot
+   *
+   *  Chooses a pivot, moves all values less than
+   *  the pivot to its left, and moves all values
+   *  greater than the pivot to its right.
+   *
+   *  @return index (location) of pivot in partitioned array
+   */
+  def partition(subArray: Array[Int], low: Int, hi: Int) : Int = {
+    val pivot = hi;
+    var i = low;
+    for (
+      j <- low to hi
+      if subArray(j) < subArray(pivot)
+    ) {swap(subArray, i, j); i+=1;}
+
+    // Lastly, move pivot value to the dividing line at i
+    swap(subArray, i, pivot);
+    return i
+  }
+}
